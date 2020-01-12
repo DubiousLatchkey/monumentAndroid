4d2e21ec0dded0b3853922d0a66bd8d19d2e1d14
Alvin Tsui <awtsui@uci.edu>
2020-01-12 05:30:54 -0800

    minor changes
    to profile

--- Monument/app/src/main/java/com/example/monument/ui/login/ProfileActivity.java
+++ Monument/app/src/main/java/com/example/monument/ui/login/ProfileActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.monument.R;
@@ -15,7 +16,7 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     private TextView currencyText;
     private Button backButton;
-    private TextView emailText;
+    private EditText emailText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
